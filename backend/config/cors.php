bf459ae3694b6748bff3ee8c90322356e983e247
ThisalC <thisalchamodya123@gmail.com>
2025-12-13 17:36:28 +0530

    slight adjustments to the deployed origin url

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -7,7 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://placement-test-thisal.vercel.app/',
+        'https://placement-test-thisal.vercel.app',
         'http://localhost:3000',
     ],
 
