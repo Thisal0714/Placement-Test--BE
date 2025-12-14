5dc9d20b565b669cf238b093595f384d2006cdd3
ThisalC <thisalchamodya123@gmail.com>
2025-12-14 18:55:58 +0530

    dockerfile added

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -19,6 +19,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $base64 = env('FIREBASE_SERVICE_ACCOUNT_BASE64');
+
+        if ($base64) {
+            $jsonPath = storage_path('app/firebase_service_account.json');
+            file_put_contents($jsonPath, base64_decode($base64));
+
+            // Make Firebase SDK find it
+            putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $jsonPath);
+        }
     }
 }
